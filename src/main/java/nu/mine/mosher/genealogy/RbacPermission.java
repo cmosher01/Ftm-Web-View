27775590403f13e3865069ce74473f67c1a95d0c
Christopher A. Mosher <cmosher01@gmail.com>
2022-06-25 21:57:42 -0400

    add CONFIG permission

--- src/main/java/nu/mine/mosher/genealogy/RbacPermission.java
+++ src/main/java/nu/mine/mosher/genealogy/RbacPermission.java
@@ -1,24 +1,5 @@
 package nu.mine.mosher.genealogy;
 
-//import java.util.*;
-
 public enum RbacPermission {
-//    PUBLIC(Set.of()),
-//    PRIVATE(Set.of(PUBLIC)),
-//    LIST(Set.of(PUBLIC)),
-//    READ(Set.of(LIST)),
-//    ;
-//
-//    private final Set<RbacPermission> implies;
-//
-//    RbacPermission(final Set<RbacPermission> implies) {
-//        this.implies = implies;
-//    }
-//
-//    public void addImpliedTo(final Collection<RbacPermission> s) {
-//        s.add(this);
-//        // this assumes graph is acyclic:
-//        this.implies.forEach(i -> i.addImpliedTo(s));
-//    }
-    PUBLIC, PRIVATE, LIST, READ
+    PUBLIC, PRIVATE, LIST, READ, CONFIG
 }
